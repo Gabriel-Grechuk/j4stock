f62f1d1217fab3037a7c27ee1233306ddc9d77fc
Gabe <gabrielgrechuk@outlook.com>
2021-09-03 12:58:53 -0300

    Removel old sources

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-import gui.*;
-
-class Main{
-	public static void main(String[] args){
-		// Test window.
-		MainWindow window = new MainWindow();
-	}
-}
--- src/build-windows.bat
+++
@@ -1,5 +0,0 @@
-mkdir Build
-javac *.java
-jar cmvf manifest.mf Build\App.jar *.class
-del *.class
-pause
